aa9edce3df48ecec409d142e9f4a34860572d75b
MilkOreo <15528377112@163.com>
2023-03-01 12:47:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU20641T0rJSiKIAESGrUYYxoJyT9n0IvSK6r2ra',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: '0003',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-18',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
   USE_PASSAGE: 'push-deer',
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
