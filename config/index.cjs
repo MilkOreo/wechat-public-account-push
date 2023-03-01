8dd338fee61fb1b4f7d29219237c5286cba9ff07
MilkOreo <15528377112@163.com>
2023-03-01 14:59:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '哒哒',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU20641T0rJSiKIAESGrUYYxoJyT9n0IvSK6r2ra',
+      id: 'PDU20644TyvuLFwQ9gRnXcMuQq5iZNMXZopfjuvOT',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0003',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
